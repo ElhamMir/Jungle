3804b7f7ba2218562d17764657a5936639cb7e08
Elham <mireshghi@yahoo.com>
2022-01-07 00:12:19 +0000

    add counts to dashboard

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],password: ENV["HTTP_BASIC_PASSWORD"]
-
+  def show
+    @product_count = Product.count
+    @category_count = Category.count
+  end
   def index
     @products = Product.order(id: :desc).all
   end
