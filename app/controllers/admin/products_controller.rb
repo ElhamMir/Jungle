f6640288c0ca6b324715a6032ba22eb9701dae98
Elham <mireshghi@yahoo.com>
2022-01-07 02:06:46 +0000

    fix route

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],password: ENV["HTTP_BASIC_PASSWORD"]
-  def show
-    @product_count = Product.count
-    @category_count = Category.count
-  end
+ 
   def index
     @products = Product.order(id: :desc).all
   end
